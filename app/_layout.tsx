b2793fb797f11ed144ceca9bcc08073c4e9bd68b
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-10-01 22:52:51 +0200

    chore: supebase setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,41 +1,65 @@
 import { Stack } from "expo-router";
 import { clients, db, powersync } from "@/src/powersync/powersync";
-import { use } from "react";
+import { PropsWithChildren, use } from "react";
 import { PowerSyncContext } from "@powersync/react";
+import "react-native-url-polyfill/auto";
+import { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import { Session } from "@supabase/supabase-js";
+import { supabase } from "@/src/supabase";
+import Auth from "@/src/components/Auth";
 
 // @ts-ignore
 window.___test = async () => {
   await db.delete(clients);
 };
 
+function AuthGuard({ children }: PropsWithChildren) {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  if (session?.user) return <>{children}</>;
+  return <Auth />;
+}
+
 const promise = powersync.init();
 export default function RootLayout() {
   use(promise);
   return (
     <PowerSyncContext.Provider value={powersync}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
+      <AuthGuard>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
 
-        <Stack.Screen
-          name="create-time-entry"
-          options={{
-            presentation: "formSheet",
-            sheetAllowedDetents: "fitToContents",
-            sheetGrabberVisible: true,
-            sheetExpandsWhenScrolledToEdge: true,
-          }}
-        />
+          <Stack.Screen
+            name="create-time-entry"
+            options={{
+              presentation: "formSheet",
+              sheetAllowedDetents: "fitToContents",
+              sheetGrabberVisible: true,
+              sheetExpandsWhenScrolledToEdge: true,
+            }}
+          />
 
-        <Stack.Screen
-          name="create-client"
-          options={{
-            presentation: "formSheet",
-            sheetAllowedDetents: "fitToContents",
-            sheetGrabberVisible: true,
-            sheetExpandsWhenScrolledToEdge: true,
-          }}
-        />
-      </Stack>
+          <Stack.Screen
+            name="create-client"
+            options={{
+              presentation: "formSheet",
+              sheetAllowedDetents: "fitToContents",
+              sheetGrabberVisible: true,
+              sheetExpandsWhenScrolledToEdge: true,
+            }}
+          />
+        </Stack>
+      </AuthGuard>
     </PowerSyncContext.Provider>
   );
 }
